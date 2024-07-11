cc0ae14128e2183b864a9df6c853477211167f57
spiritclaw <lea@cherpillod.com>
2024-07-11 17:05:59 +0100

    [CI/CD] package build workflow

---
+++ package.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+PACKAGE_NAME=$1
+VERSION=$2
+PACKAGE_ARCH=$3
+DIR_NAME="${PACKAGE_NAME}_${VERSION}-1_${PACKAGE_ARCH}"
+
+rm -rf .package-build
+mkdir .package-build
+cd .package-build
+
+mkdir $DIR_NAME
+mkdir -p $DIR_NAME/etc/presentium/rfid-reader-service
+mkdir -p $DIR_NAME/etc/systemd/system
+mkdir -p $DIR_NAME/DEBIAN
+
+echo "Package: $PACKAGE_NAME
+Version: $VERSION
+Maintainer: <info@presentium.ch>
+Depends: python3
+Architecture: $PACKAGE_ARCH
+Homepage: https://presentium.ch
+Description: Exposes an RFID reader as a simple rest API. Used in the presentium stack." \
+> $DIR_NAME/DEBIAN/control
+
+echo "#!/bin/bash
+python3 -m pip install -r requirements.txt" \
+> $DIR_NAME/DEBIAN/postinst
+chmod 555 $DIR_NAME/DEBIAN/postinst
+
+cp ../main.py $DIR_NAME/etc/presentium/rfid-reader-service
+cp ../requirements.txt $DIR_NAME/etc/presentium/rfid-reader-service
+
+cp ../presentium-rfid-reader.service $DIR_NAME/etc/systemd/system
+
+dpkg --build $DIR_NAME
+dpkg-deb --info $DIR_NAME.deb > "$DIR_NAME.deb.info"
+dpkg-deb --contents $DIR_NAME.deb > "$DIR_NAME.deb.content"
