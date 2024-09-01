10997a666e89407919a0600907bcfb2b39d097cc
Christophe - CC4 <roulin43@gmail.com>
2024-09-01 17:40:45 +0200

    feat: update paths and package build script

--- package.sh
+++ package.sh
@@ -8,10 +8,14 @@ rm -rf .package-build
 mkdir .package-build
 cd .package-build
 
+# ------------------------------------------------------------
+
 mkdir $DIR_NAME
 mkdir -p $DIR_NAME/etc/presentium/presentium-client
 mkdir -p $DIR_NAME/etc/systemd/system
 mkdir -p $DIR_NAME/DEBIAN
+
+# ------------------------------------------------------------
 
 echo "Package: $PACKAGE_NAME
 Version: $VERSION
@@ -22,18 +26,38 @@ Homepage: https://presentium.ch
 Description: Presentium client app." \
 > $DIR_NAME/DEBIAN/control
 
+# ------------------------------------------------------------
+
 echo "#!/bin/bash
 cd /etc/presentium/presentium-client
-python3 -m venv presentium
-. ./presentium/bin/activate
-python3 -m pip install -r requirements.txt" \
-python3 /etc/presentium/presentium-client/codegen.py \
+python3 -m venv presentium-venv
+. ./presentium-venv/bin/activate
+pip3 install -r requirements.txt" \
+python3 codegen.py \
 > $DIR_NAME/DEBIAN/postinst
 chmod 555 $DIR_NAME/DEBIAN/postinst
 
-cp -r ../* $DIR_NAME/etc/presentium/presentium-client
+# ------------------------------------------------------------
 
+# Main files
+cp ../main.py $DIR_NAME/etc/presentium/presentium-client
+cp ../requirements.txt $DIR_NAME/etc/presentium/presentium-client
+cp ../codegen.py $DIR_NAME/etc/presentium/presentium-client
+
+# Python modules
+cp -r ../src $DIR_NAME/etc/presentium/presentium-client
+
+# gRPC/protobuf protos
+cp -r ../protos $DIR_NAME/etc/presentium/presentium-client
+
+# Copy config files and create a default config.ini
+cp -r ../config $DIR_NAME/etc/presentium/presentium-client
+cp $DIR_NAME/etc/presentium/presentium-client/config/config.ini.default $DIR_NAME/etc/presentium/presentium-client/config/config.ini
+
+# Copy systemd service file
 cp ../presentium-client.service $DIR_NAME/etc/systemd/system
+
+# ------------------------------------------------------------
 
 dpkg --build $DIR_NAME
 dpkg-deb --info $DIR_NAME.deb > "$DIR_NAME.deb.info"
